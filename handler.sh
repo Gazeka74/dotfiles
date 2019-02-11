11f105bbd7a3aaad0b1e5d836a831076151b4ee9
gazeka74 <gazeka74@gmail.com>
2019-02-11 10:52:19 +0100

    AUTO COMMIT : lun. févr. 11 10:52:19 CET 2019

--- handler.sh
+++ handler.sh
@@ -12,7 +12,7 @@ OPTIONS="Options:
 
 function backup {
 
-    printf "\n   BACKUPING FILES FOR THE USER : $WHOAMI\n\n"
+    printf "\n===> BACKUPING FILES FOR THE USER : $WHOAMI\n\n"
 
     echo "=> backuping i3"
     mkdir -p $WHEREAMI/i3
@@ -38,7 +38,12 @@ function backup {
 
     echo "=> backuping the background"
     mkdir -p $WHEREAMI/background/
-    cp /home/loris/Images/background/background.png $WHEREAMI/background/background.png
+    cp /home/$WHOAMI/Images/background/background.png $WHEREAMI/background/background.png
+
+    echo "=> backuping rofi"
+    mkdir -p $WHEREAMI/rofi/themes
+    cp -r /usr/share/rofi/themes $WHEREAMI/rofi/themes/
+    cp ~/.config/rofi/config $WHEREAMI/rofi/config
 
     printf "=> pushing to git \n\n"
 
@@ -48,16 +53,51 @@ function backup {
 }
 
 function restore {
-    printf "\n   RESTORING FILES FOR THE USER : $WHOAMI\n\n"
+    printf "\n===> RESTORING FILES FOR THE USER : $WHOAMI\n\n"
+
+    echo "=> restoring i3"
+    mkdir -p ~/.config/i3
+    cp $WHEREAMI/i3/config ~/.config/i3/config
+
+    echo "=> restoring polybar"
+    mkdir -p ~/.config/polybar/
+    cp $WHEREAMI/polybar/config ~/.config/polybar/config
+    cp $WHEREAMI/polybar/master.conf ~/.config/polybar/master.conf
+    cp $WHEREAMI/polybar/modules.conf ~/.config/polybar/modules.conf
+
+    echo "=> restoring zshrc"
+    cp $WHEREAMI/.zshrc ~/.zshrc
+
+    echo "=> restoring termite"
+    mkdir -p ~/.config/termite/
+    cp $WHEREAMI/termite/config ~/.config/termite/config
+
+    echo "=> restoring i3lock-fancy (require root rights)"
+    sudo mkdir -p /usr/share/i3lock-fancy
+    sudo cp -r $WHEREAMI/locker/usrshare/i3lock-fancy /usr/share/i3lock-fancy
+    sudo cp $WHEREAMI/locker/i3lock-fancy /usr/bin/i3lock-fancy 
+
+    echo "=> restoring the background"
+    mkdir -p /home/$WHOAMI/Images/background/
+    cp $WHEREAMI/background/background.png /home/$WHOAMI/Images/background/background.png
+
+    echo "=> restoring rofi"
+    sudo mkdir -p /usr/share/rofi/themes
+    sudo cp -r $WHEREAMI/rofi/themes /usr/share/rofi/themes
+    cp  $WHEREAMI/rofi/config ~/.config/rofi/config
+
+}
+
+function print_usage {
+    printf "Usage:\n     $(basename $0) [options]\n\n$OPTIONS\n\n"
 }
 while true; do
     if [ -z "$1" ]; then
-        printf "Usage: $(basename $0) [options]\n\n$OPTIONS\n\n" ; exit 1
+        print_usage ; exit 1
     fi
-
     case "$1" in
         -h|--help)
-            printf "Usage: $(basename $0) [options]\n\n$OPTIONS\n\n" ; exit 1 ;;
+            print_usage ; exit 1 ;;
         -b|--backup)
             backup; exit 0;;
         -r|--restore)
